1d60ee1d05f0b2f227bd79c6bc05d2d94384f6c4
purwar1997 <shubhampurwar511@gmail.com>
2022-09-20 21:58:47 +0530

    Added call_apply_bind.js and polyfills.js

---
+++ call_apply_bind.js
@@ -0,0 +1,74 @@
+// In JS, functions are objects. So they can have their own properties and methods.
+// call(), apply() and bind() are methods of functions
+
+const user1 = {
+  name: 'Shubham Purwar',
+  course: 'BCA',
+};
+
+const user2 = {
+  name: 'Akshay Saini',
+  course: 'BTech',
+};
+
+function userInfo(age, college) {
+  return `${this.name}, ${age} years old is currently pursuing ${this.course} from ${college}`;
+}
+
+// call() and apply() will directly invoke the function
+
+// call() method
+// Syntax: function.call(object, arg1, arg2, ...)
+
+// first argument must be an object
+console.log(userInfo.call(user1, 24, 'IIT Delhi'));
+console.log(userInfo.call(user2, 26, 'NIT Bhopal'));
+
+// apply() method
+// Syntax: function.apply(object, arrayOfValues)
+
+console.log(userInfo.apply(user1, [24, 'IGNOU Delhi']));
+console.log(userInfo.apply(user2, [26, 'BITS Pilani']));
+
+// bind() method
+// Syntax: function.bind(object, arg1, arg2, ...)
+// bind() doesn't invokes function. Instead, it returns a copy of the function that can be invoked later.
+
+const student1 = {
+  name: 'Shubham Purwar',
+  grade: '12th',
+  rollNo: 45,
+};
+
+const student2 = {
+  name: 'Mansi Purwar',
+  grade: '10th',
+  rollNo: 41,
+};
+
+const student3 = {
+  name: 'Anurag Pandey',
+  grade: '11th',
+  rollNo: 20,
+};
+
+function marks(mathsMarks, scienceMarks, englishMarks, hindiMarks) {
+  this.maths = mathsMarks;
+  this.science = scienceMarks;
+  this.englisg = englishMarks;
+  this.hindi = hindiMarks;
+}
+
+// stu1 and stu2 will contain a copy of the marks() function
+
+// in stu1, 'this' will refer to student1 object
+const stu1 = marks.bind(student1, 95, 90);
+stu1(80, 75);
+
+console.table(student1);
+
+// in stu2, 'this' will refer to student2 object
+const stu2 = marks.bind(student2);
+stu2(70, 90, 60, 87);
+
+console.table(student2);
---
+++ polyfills.js
@@ -0,0 +1,76 @@
+const user = {
+  name: 'Shubham Purwar',
+  course: 'BCA',
+};
+
+function userInfo(age, college) {
+  return `${this.name}, ${age} years old is currently pursuing ${this.course} from ${college}`;
+}
+
+// polyfill or call()
+Function.prototype.myCall = function (object, ...args) {
+  object.func = this;
+  return object.func(...args);
+};
+
+console.log(userInfo.myCall(user, 24, 'VIT Bhopal'));
+
+// polyfill for apply()
+Function.prototype.myApply = function (object, args) {
+  object.func = this;
+  return object.func(...args);
+};
+
+console.log(userInfo.myApply(user, [21, 'BIT Mesra']));
+
+const student = {
+  name: 'Shubham Purwar',
+  grade: '12th',
+  rollNo: 45,
+};
+
+function marks(mathsMarks, scienceMarks, englishMarks, hindiMarks) {
+  this.maths = mathsMarks;
+  this.science = scienceMarks;
+  this.englisg = englishMarks;
+  this.hindi = hindiMarks;
+}
+
+// polyfill for bind()
+Function.prototype.myBind = function (object, ...args) {
+  object.func = this;
+  return function (...args_) {
+    return object.func(...args, ...args_);
+  };
+};
+
+let stu = marks.myBind(student, 100);
+stu(90, 80, 40);
+
+console.table(student);
+
+// polyfill for bind() using call() method
+Function.prototype.myBind = function (object, ...args) {
+  const func = this;
+  return function (...args_) {
+    return func.call(object, ...args, ...args_);
+  };
+};
+
+stu = marks.myBind(student, 100, 90, 80);
+stu(49);
+
+console.table(student);
+
+// polyfill for bind() using apply() method
+Function.prototype.myBind = function (object, ...args) {
+  const func = this;
+  return function (...args_) {
+    return func.apply(object, [...args, ...args_]);
+  };
+};
+
+stu = marks.myBind(student, 100);
+stu(49, 90, 80);
+
+console.table(student);
